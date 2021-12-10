df6fbd9306119a75270df10ff86dc56c0d4009f3
BrianLi23 <animeshack123@gmail.com>
2021-12-10 14:15:28 -0500

    Finished Product

--- Sketch.java
+++ Sketch.java
@@ -16,20 +16,59 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
+	// Green background
     background(210, 255, 173);
+    
+    // Blue background
+    noStroke();
+    fill(153, 204, 255);
+    rect(0, 0, 600, 300);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+	 
+	 // Cloud #1
+	 noStroke();
+	 fill(255, 255, 255);
+	 ellipse(100, 75, 40, 40);
+	 ellipse(115, 90, 40, 40);
+	 ellipse(85, 90, 40, 40);
+	 ellipse(120, 75, 40, 40);
+	 ellipse(135, 90, 40, 40);
+	 
+	 // Cloud 2
+	 noStroke();
+	 fill(255, 255, 255);
+	 ellipse(450, 115, 40, 40);
+	 ellipse(465, 130, 40, 40);
+	 ellipse(435, 130, 40, 40);
+	 ellipse(470, 115, 40, 40);
+	 ellipse(485, 130, 40, 40);
+	 
+	 // House Structure (Rectangle and Triangle)
+	 noStroke();
+	 fill(102, 0, 0);
+	 rect(150, 250, 250, 200);
+	 
+	 noStroke();
+	 fill(153, 0, 0);
+	 triangle(150, 250, 400, 250, 275, 100);
+	 
+	 // Door
+	 stroke(0, 0, 0);
+	 fill(153, 76, 0);
+	 rect(275, 349, 65, 100);
+	 
+	 fill(204, 204, 0);
+	 ellipse(330, 400, 10, 10);
+	 
+	 // Sun
+	 fill(255, 255, 51);
+	 ellipse(600, 0, 100, 100);
+	 
   }
   
   // define other methods down here.
